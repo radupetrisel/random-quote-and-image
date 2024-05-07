e066473c6859997d8aa087e54cb93a08fe585097
Radu Petrisel <radupetrisel@gmail.com>
2024-05-07 13:36:39 +0300

    Initial Commit

---
+++ random-quote-and-image/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  random-quote-and-image
+//
+//  Created by Radu Petrisel on 07.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ random-quote-and-image/random_quote_and_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_quote_and_imageApp.swift
+//  random-quote-and-image
+//
+//  Created by Radu Petrisel on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct random_quote_and_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
